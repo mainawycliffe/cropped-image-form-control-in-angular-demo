aced9231567d4d3fd4efa980f3e4d71acb95145c
Maina Wycliffe <wmmaina7@gmail.com>
2018-08-17 17:26:58 +0300

    added npm packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxCroppieModule } from 'ngx-croppie';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatIconModule,
+  MatButtonModule,
+  MatFormFieldModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { CustomImageFormControlComponent } from './custom-image-form-control/custom-image-form-control.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CustomImageFormControlComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxCroppieModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
